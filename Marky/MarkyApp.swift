3f724d91b1a7e662332142d1885a6fd72019e467
conistuffwow <deatu013@gmail.com>
2025-06-30 13:24:11 -0400

    Initial Commit

---
+++ Marky/MarkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkyApp.swift
+//  Marky
+//
+//  Created by Coni on 2025-06-30.
+//
+
+import SwiftUI
+
+@main
+struct MarkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
